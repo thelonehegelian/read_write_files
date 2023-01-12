2cf3a9cdc49e1e0099db49c08e1773f3318c10bd
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-12 22:52:04 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use same_file::Handle;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+fn main() {
+    let file = Path::new("poem.txt");
+    let std_handle = Handle::stdout().unwrap();
+    let handle = Handle::from_path(&file).unwrap();
+
+    if std_handle == handle {
+        println!("stdout is the same as poem.txt");
+    } else {
+        let file = File::open(file).unwrap();
+        let reader = BufReader::new(file);
+        for (num, line) in reader.lines().enumerate() {
+            println!("{}: {}", num, line.unwrap());
+        }
+    }
+}
