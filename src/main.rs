77ba0cf85261770265cf13e56c769e9faa8ebeee
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-12 23:20:56 +0000

    Update comment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use memmap::Mmap;
 use same_file::Handle;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 fn main() {
+    /*************************************************
+     * PREVENTS READING AND WRITING FROM THE SAME FILE
+     *************************************************/
+
     let file = Path::new("text.txt");
     let std_handle = Handle::stdout().unwrap();
     let handle = Handle::from_path(&file).unwrap();
@@ -17,4 +22,15 @@ fn main() {
             println!("{}: {}", num, line.unwrap());
         }
     }
+
+    /*******************************
+     * OPEN FILE USING A MEMORY MAP
+     ******************************/
+
+    let file = File::open("text.txt").unwrap();
+    // The Mmap::map function assumes the file behind
+    // the memory map is not being modified at the same
+    // time by another process or else a race condition occurs.
+    let map = unsafe { Mmap::map(&file).unwrap() };
+    print!("{:?}", map);
 }
